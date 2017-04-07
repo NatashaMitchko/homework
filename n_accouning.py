43ec19b71ef19f23cb013d8e555032d432068fba
vagrant <vagrant@vagrant.vm>
2017-04-07 04:33:11 +0000

    Adding data and working files to repository. n_accounting.py will be a refactored version of accounting.py.

---
+++ n_accouning.py
@@ -0,0 +1,27 @@
+# We sold 4795 Musk melons at 1.15 each for a total of 5514.25
+# We sold 34982 Watermelon melons at 1.75 each for a total of 61218.50
+# We sold 1928 Hybrid melons at 1.30 each for a total of 2506.40
+# We sold 841 Winter melons at 4.00 each for a total of 3364.00
+# ******************************************
+# Salespeople generated $206266.50 in revenue.
+# Internet sales generated $108440.29 in revenue.
+# Guess there's some value to those salespeople after all.
+
+# We want to know for each type of melon: how many sold, unit cost and total cost
+# We also want to know how much money was made by sales vs online sales
+# "Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+
+melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+
+def extract_from_melon_data(file_name):
+    melon_data = open(file_name)
+    for line in melon_data:
+        data = line.split('|')
+
+
+
+def melon_counter():
+
+melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00}
+
+def extract_from_sales_data():
